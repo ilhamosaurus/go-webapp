971d50c56b8cfc682cf07db2c9a141e92a1188c4
Muhammad Ilham Karisma <ilhamsssa@gmail.com>
2024-07-08 15:23:47 +0700

    first web app with db

---
+++ bookstore/cmd/main/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	fmt.Println("Server is listening on port 9010...")
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
